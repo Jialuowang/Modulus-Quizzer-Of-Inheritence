99bbf6ca7b0d9a358da2e18fa4aab2d831c10660
jialuo.wang <jialuo.wang@mullenmail.com>
2018-01-11 14:34:31 -0700

    origin

---
+++ Modulus Quizzer of Inheritence/src/runner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class runner
+	{
+
+		public static void main(String[] args)
+			{
+				Scanner question = new Scanner(System.in);
+				System.out.println("How many question you want?(at least 3 question)");
+				int numberOfQuestion = question.nextInt();
+				for(int n = 0; n < numberOfQuestion; n++)
+					{
+						
+					}
+			}
+
+	}
